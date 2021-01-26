37884f15318672f5ebdd90c41cbc821ec11b7b48
ggotfm <ggo@topfloormarketing.net>
2021-01-26 17:06:01 -0600

    Main Page class

---
+++ src/base.page.ts
@@ -0,0 +1,19 @@
+import logger from '@wdio/logger';
+
+export default class BasePage {
+    log:any
+    constructor(){
+        this.log = logger('page-object-model');
+    }
+    
+
+    logger() { return this.log; }
+
+    /**
+     * Opens page
+     */
+    open(path: string) {
+        this.log.debug(`Open site "${path}"`);
+        browser.url(path);
+    }
+}
